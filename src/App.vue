892c00c5828694c52cfccff42f9155dcd96ca1cd
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-06-23 20:57:04 +0700

    new task insert row

--- src/App.vue
+++ src/App.vue
@@ -4,51 +4,61 @@ import BaseHeading from './components/base/base-heading.vue';
 import BaseButton from './components/base/base-button.vue';
 import BaseTable from './components/base/base-table.vue';
 import BaseSelect from './components/base/base-select.vue';
+import BaseInput from './components/base/base-input.vue';
 import { h, ref } from 'vue';
 import dayjs from 'dayjs';
 
 const tasks = ref([
   {
+    id: 'Setup linter',
     name: 'Setup linter',
     deadline: new Date(),
     status: 'todo',
   },
   {
+    id: 'Setup tailwind',
     name: 'Setup tailwind',
     deadline: new Date(),
     status: 'todo',
   },
   {
+    id: 'Home page',
     name: 'Home page',
     deadline: new Date(),
     status: 'todo',
   },
   {
+    id: 'Task table',
     name: 'Task table',
     deadline: new Date(),
     status: 'todo',
   },
   {
+    id: 'Task table from store',
     name: 'Task table from store',
     deadline: new Date(),
     status: 'todo',
   },
   {
+    id: 'Store task',
     name: 'Store task',
     deadline: new Date(),
     status: 'todo',
   },
   {
+    id: 'Edit task',
     name: 'Edit task',
     deadline: new Date(),
     status: 'todo',
   },
   {
+    id: 'Delete task',
     name: 'Delete task',
     deadline: new Date(),
     status: 'todo',
   },
   {
+    id: 'Update task status',
     name: 'Update task status',
     deadline: new Date(),
     status: 'todo',
@@ -56,11 +66,21 @@ const tasks = ref([
 ]);
 
 const columns = [
-  { id: 'name', name: 'Name' },
+  {
+    id: 'name',
+    name: 'Name',
+    render: ({ item }) =>
+      item.id
+        ? h('p', item.name)
+        : h(BaseInput, { placeholder: 'Enter task name' }),
+  },
   {
     id: 'deadline',
     name: 'Deadline',
-    value: (item) => dayjs(item.deadline).format('DD MMMM YYYY'),
+    render: ({ item }) =>
+      item.id
+        ? h('p', dayjs(item.deadline).format('DD MMMM YYYY'))
+        : h(BaseInput, { placeholder: 'Enter task name', type: 'date' }),
   },
   {
     id: 'status',
@@ -77,6 +97,15 @@ const columns = [
       }),
   },
 ];
+
+function onNewTask() {
+  tasks.value.push({
+    id: null,
+    name: null,
+    deadline: null,
+    status: 'todo',
+  });
+}
 </script>
 
 <template>
@@ -86,7 +115,10 @@ const columns = [
       :level="3"
     >
       <template #action>
-        <base-button color="blue">
+        <base-button
+          color="blue"
+          @click="onNewTask"
+        >
           New Task
         </base-button>
       </template>
