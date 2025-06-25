8a25879fb95dd58c948d7be30ba512b37cdcff8a
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-06-25 19:55:45 +0700

    base input expose input

--- src/components/base/base-input.vue
+++ src/components/base/base-input.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue';
 
+const value = defineModel();
 const input = ref();
 
 defineExpose({ input });
@@ -9,6 +10,7 @@ defineExpose({ input });
 <template>
   <input
     ref="input"
+    v-model="value"
     type="text"
     class="border border-gray-300 h-10 px-2.5 rounded-md appearance-none text-gray-900 focus:outline-blue-600"
   >
