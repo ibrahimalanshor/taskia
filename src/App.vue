6afa9adbe151604287dde714002e39a488463db7
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-06-25 19:55:26 +0700

    save new task

--- src/App.vue
+++ src/App.vue
@@ -5,64 +5,73 @@ import BaseButton from './components/base/base-button.vue';
 import BaseTable from './components/base/base-table.vue';
 import BaseSelect from './components/base/base-select.vue';
 import BaseInput from './components/base/base-input.vue';
-import { h, nextTick, ref } from 'vue';
+import { computed, h, nextTick, ref } from 'vue';
 import dayjs from 'dayjs';
 
-const taskNameInput = ref([]);
+const taskNameInput = ref({});
 const tasks = ref([
   {
     id: 'Setup linter',
     name: 'Setup linter',
     deadline: new Date(),
     status: 'todo',
+    saved: true,
   },
   {
     id: 'Setup tailwind',
     name: 'Setup tailwind',
     deadline: new Date(),
     status: 'todo',
+    saved: true,
   },
   {
     id: 'Home page',
     name: 'Home page',
     deadline: new Date(),
     status: 'todo',
+    saved: true,
   },
   {
     id: 'Task table',
     name: 'Task table',
     deadline: new Date(),
     status: 'todo',
+    saved: true,
   },
   {
     id: 'Task table from store',
     name: 'Task table from store',
     deadline: new Date(),
     status: 'todo',
+    saved: true,
   },
   {
     id: 'Store task',
     name: 'Store task',
     deadline: new Date(),
     status: 'todo',
+    saved: true,
   },
   {
     id: 'Edit task',
     name: 'Edit task',
     deadline: new Date(),
     status: 'todo',
+    saved: true,
   },
   {
     id: 'Delete task',
     name: 'Delete task',
     deadline: new Date(),
     status: 'todo',
+    saved: true,
   },
   {
     id: 'Update task status',
     name: 'Update task status',
     deadline: new Date(),
     status: 'todo',
+    saved: true,
   },
 ]);
 
@@ -70,26 +79,34 @@ const columns = [
   {
     id: 'name',
     name: 'Name',
-    render: ({ item }) =>
-      item.id
+    render: ({ item, index }) =>
+      item.saved
         ? h('p', item.name)
         : h(BaseInput, {
-            ref: setTaskNameInputRef,
+            ref: (el) => setTaskNameInputRef(el, item.id),
             placeholder: 'Enter task name',
+            modelValue: tasks.value[index].name,
+            'onUpdate:modelValue': (value) => (tasks.value[index].name = value),
           }),
   },
   {
     id: 'deadline',
     name: 'Deadline',
-    render: ({ item }) =>
-      item.id
+    render: ({ item, index }) =>
+      item.saved
         ? h('p', dayjs(item.deadline).format('DD MMMM YYYY'))
-        : h(BaseInput, { placeholder: 'Enter task name', type: 'date' }),
+        : h(BaseInput, {
+            placeholder: 'Enter task name',
+            type: 'date',
+            modelValue: tasks.value[index].deadline,
+            'onUpdate:modelValue': (value) =>
+              (tasks.value[index].deadline = value),
+          }),
   },
   {
     id: 'status',
     name: 'Status',
-    render: ({ item, index }) =>
+    render: ({ index }) =>
       h(BaseSelect, {
         options: [
           { id: 'todo', name: 'To Do' },
@@ -97,28 +114,51 @@ const columns = [
           { id: 'done', name: 'Done' },
         ],
         modelValue: tasks.value[index].status,
-        'update:modelValue': (value) => (tasks.value[index].status = value),
+        'onUpdate:modelValue': (value) => (tasks.value[index].status = value),
       }),
+  },
+  {
+    id: 'action',
+    name: 'Action',
+    render: ({ item, index }) =>
+      h(
+        'div',
+        { class: 'flex gap-1' },
+        !item.saved
+          ? [
+              h(
+                BaseButton,
+                { color: 'blue', size: 'sm', onClick: () => onSaveTask(index) },
+                () => 'Save',
+              ),
+              h(BaseButton, { size: 'sm' }, () => 'Cancel'),
+            ]
+          : [],
+      ),
   },
 ];
 
-function setTaskNameInputRef(el) {
-  taskNameInput.value.push(el);
+function setTaskNameInputRef(el, index) {
+  taskNameInput.value[index] = el;
 }
 
 async function onNewTask() {
-  const newIndex = tasks.value.filter((task) => !task.id).length;
+  const id = Date.now();
 
   tasks.value.push({
-    id: null,
+    id,
     name: null,
     deadline: null,
     status: 'todo',
+    saved: false,
   });
 
   await nextTick();
 
-  taskNameInput.value[newIndex].input.focus();
+  taskNameInput.value[id].input.focus();
+}
+function onSaveTask(index) {
+  tasks.value[index].saved = true;
 }
 </script>
 
