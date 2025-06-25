3a0d5ffc9c37e46784f470a5a382ad84243b8084
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-06-25 16:32:09 +0700

    input ref

--- src/components/base/base-input.vue
+++ src/components/base/base-input.vue
@@ -1,5 +1,14 @@
+<script setup>
+import { ref } from 'vue';
+
+const input = ref();
+
+defineExpose({ input });
+</script>
+
 <template>
   <input
+    ref="input"
     type="text"
     class="border border-gray-300 h-10 px-2.5 rounded-md appearance-none text-gray-900 focus:outline-blue-600"
   >
