139462fa7a2b7b8dcacec54efffe8ab8f6d158d6
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-06-27 15:35:21 +0700

    base select size

--- src/components/base/base-select.vue
+++ src/components/base/base-select.vue
@@ -1,19 +1,44 @@
 <script setup>
-defineProps({
+import { computed } from 'vue';
+
+const props = defineProps({
   options: {
     type: Array,
     required: true,
   },
+  size: {
+    type: String,
+    default: 'md',
+    validator: (value) => ['sm', 'md', 'lg'].includes(value),
+  },
 });
 
 const selected = defineModel();
+
+const size = computed(() => {
+  return {
+    sm: 'h-8 text-sm px-3 pr-10',
+    md: 'h-10 px-2.5 pr-10',
+    lg: 'h-12 text-lg px-4 pr-10',
+  }[props.size || 'md'];
+});
+const chevronSize = computed(() => {
+  return {
+    sm: 'size-3 absolute top-2.5 right-2',
+    md: 'size-4 absolute top-3 right-2.5',
+    lg: 'size-4 absolute top-4 right-3',
+  }[props.size || 'md'];
+});
 </script>
 
 <template>
   <div class="relative w-fit">
     <select
       v-model="selected"
-      class="border border-gray-300 h-10 px-2.5 pr-10 rounded-md appearance-none text-gray-900 focus:outline-blue-600"
+      :class="[
+        'border border-gray-300 rounded-md appearance-none text-gray-900 focus:outline-blue-600',
+        size,
+      ]"
     >
       <option
         v-for="option in options"
@@ -29,7 +54,7 @@ const selected = defineModel();
       viewBox="0 0 24 24"
       stroke-width="1.5"
       stroke="currentColor"
-      class="size-4 absolute top-3 right-2.5"
+      :class="chevronSize"
     >
       <path
         stroke-linecap="round"
