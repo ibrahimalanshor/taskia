f9a506fa4aeb8878edede499ebdb877a00b8f003
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-07-02 19:40:24 +0700

    base modal

---
+++ src/components/base/base-modal.vue
@@ -0,0 +1,39 @@
+<script setup>
+import BaseContainer from './base-container.vue';
+import BaseCard from './base-card.vue';
+
+defineProps({
+  withContainer: {
+    type: Boolean,
+    default: true,
+  },
+  classes: {
+    type: Object,
+    default: () => ({
+      container: '',
+    }),
+  },
+  cardProps: null,
+});
+
+const visible = defineModel('visible');
+</script>
+
+<template>
+  <div
+    v-if="visible"
+    class="fixed inset-0 bg-black/30 py-20"
+  >
+    <component
+      :is="withContainer ? BaseContainer : 'div'"
+      :class="classes.container"
+    >
+      <base-card
+        :bordered="false"
+        v-bind="cardProps"
+      >
+        <slot />
+      </base-card>
+    </component>
+  </div>
+</template>
