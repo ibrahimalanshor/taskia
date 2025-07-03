3e45c6b6ad25a158ff3a32e50b3da227ca5e3eb4
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-07-03 13:15:30 +0700

    instal click outside

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 import { MotionPlugin } from '@vueuse/motion';
+import vClickOutside from 'click-outside-vue3';
 
 const app = createApp(App);
 
 app.use(MotionPlugin);
+app.use(vClickOutside);
 
 app.mount('#app');
