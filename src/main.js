bae77496546b9504d6f14d3687b01caa196fd5b2
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-07-03 14:42:14 +0700

    setup router

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import './style.css';
 import App from './App.vue';
 import { MotionPlugin } from '@vueuse/motion';
 import vClickOutside from 'click-outside-vue3';
+import { router } from './router/router';
 
 const app = createApp(App);
 
 app.use(MotionPlugin);
 app.use(vClickOutside);
+app.use(router);
 
 app.mount('#app');
