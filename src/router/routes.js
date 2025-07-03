833fab863dddd268b60b2196b94f6498b5b48836
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-07-03 21:19:32 +0700

    login page

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,3 +1,4 @@
 import { homeRoutes } from 'src/features/home/home.routes';
+import { authRoutes } from 'src/features/auth/auth.routes';
 
-export const routes = [...homeRoutes];
+export const routes = [...homeRoutes, ...authRoutes];
