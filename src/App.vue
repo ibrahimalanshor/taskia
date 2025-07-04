4173708ca3606feea2d3ca949808749958413358
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-07-04 21:52:57 +0700

    dynamic title

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,17 @@
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+router.beforeEach((to) => {
+  if (to.meta.title) {
+    const appName = import.meta.env.VITE_APP_NAME;
+
+    document.title = `${appName} - ${to.meta.title}`;
+  }
+});
+</script>
+
 <template>
   <div class="min-h-screen bg-gray-100">
     <router-view />
