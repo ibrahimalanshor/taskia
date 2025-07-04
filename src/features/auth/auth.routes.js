9bf5857d3b55bb9ba9ae0770f12414e17618d77e
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-07-04 21:37:28 +0700

    login guest

--- src/features/auth/auth.routes.js
+++ src/features/auth/auth.routes.js
@@ -2,6 +2,9 @@ export const authRoutes = [
   {
     path: '/login',
     name: 'login',
+    meta: {
+      guest: true,
+    },
     component: () => import('./pages/login-page.vue'),
   },
 ];
--- src/features/auth/auth.store.js
+++ src/features/auth/auth.store.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
 export const useAuthStore = defineStore('auth', () => {
-  const loggedIn = ref(true);
+  const loggedIn = ref(false);
 
   return {
     loggedIn,
