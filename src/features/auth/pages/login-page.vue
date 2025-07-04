3c90fe359fa832f316367a4c1836b13d270f8dc2
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-07-04 16:10:04 +0700

    login request

--- src/features/auth/pages/login-page.vue
+++ src/features/auth/pages/login-page.vue
@@ -2,12 +2,44 @@
 import BaseContainer from 'src/components/base/base-container.vue';
 import BaseCard from 'src/components/base/base-card.vue';
 import BaseButton from 'src/components/base/base-button.vue';
-import { googleAuthCodeLogin } from 'vue3-google-login';
+import BaseAlert from 'src/components/base/base-alert.vue';
+// import { googleAuthCodeLogin } from 'vue3-google-login';
+import { reactive, ref } from 'vue';
+import { request } from 'src/lib/http';
 
+const loadingGoogleLogin = ref(false);
+const error = reactive({
+  visible: false,
+  message: null,
+});
+
+function googleAuthCodeLogin() {
+  return Promise.resolve({
+    code: '4/0AVMBsJh1zF9KdqYosfE4EmIErIDUysCMeh098FeFjesWMF6RivRSdjzhJfiMct0U0JVuKg',
+    scope:
+      'email profile https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email openid',
+    authuser: '0',
+    prompt: 'consent',
+  });
+}
 async function onGoogleLogin() {
-  const res = await googleAuthCodeLogin();
+  loadingGoogleLogin.value = true;
 
-  console.log(res);
+  const googleInfo = await googleAuthCodeLogin();
+  const [, err] = await request({
+    url: '/login/google',
+    method: 'post',
+    data: {
+      code: googleInfo.code,
+    },
+  });
+
+  if (err) {
+    error.visible = true;
+    error.message = err;
+  }
+
+  loadingGoogleLogin.value = false;
 }
 </script>
 
@@ -23,10 +55,19 @@ async function onGoogleLogin() {
             Welcome! Log in with your social account to begin. It's quick and
             easy.
           </p>
+          <base-alert
+            v-if="error.visible"
+            with-close
+            color="red"
+            @close="error.visible = false"
+          >
+            {{ error.message }}
+          </base-alert>
           <base-button
             color="white-bordered"
             fullwidth
             icon="tabler:brand-google-filled"
+            :loading="loadingGoogleLogin"
             @click="onGoogleLogin"
           >
             Login using Google
