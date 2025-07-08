3ddf39acf590c887c1f5886834ba133b4b8d3067
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-07-08 11:11:07 +0700

    login handle

--- src/features/auth/auth.store.js
+++ src/features/auth/auth.store.js
@@ -4,7 +4,12 @@ import { ref } from 'vue';
 export const useAuthStore = defineStore('auth', () => {
   const loggedIn = ref(false);
 
+  function login() {
+    loggedIn.value = true;
+  }
+
   return {
     loggedIn,
+    login,
   };
 });
