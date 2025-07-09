3698b2eafac6c8c98688c5ffbe214cd7ab6352e0
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-07-09 15:41:03 +0700

    auth token

--- src/features/auth/auth.store.js
+++ src/features/auth/auth.store.js
@@ -5,14 +5,22 @@ export const useAuthStore = defineStore(
   'auth',
   () => {
     const loggedIn = ref(false);
+    const accessToken = ref('null');
 
-    function login() {
+    function login(token) {
       loggedIn.value = true;
+      accessToken.value = token;
+    }
+    function logout() {
+      loggedIn.value = false;
+      accessToken.value = null;
     }
 
     return {
+      accessToken,
       loggedIn,
       login,
+      logout,
     };
   },
   { persist: true },
