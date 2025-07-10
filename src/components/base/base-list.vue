7ebe6fb2dc3a805d510ecf3a463b4364b67b9bc7
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-07-10 14:39:14 +0700

    base list header

--- src/components/base/base-list.vue
+++ src/components/base/base-list.vue
@@ -4,16 +4,24 @@ import BaseCard from './base-card.vue';
 defineProps({
   data: Array,
 });
+
+const classes = {
+  item: 'p-4',
+};
 </script>
 
 <template>
   <base-card paddless>
     <div class="divide-y divide-gray-300">
-      <slot :classes="{ item: 'p-4' }">
+      <slot
+        name="header"
+        :classes="{ item: classes.item }"
+      />
+      <slot :classes="{ item: classes.item }">
         <div
           v-for="(item, index) in data"
           :key="item.id"
-          class="p-4"
+          :class="classes.item"
         >
           <slot
             name="item"
