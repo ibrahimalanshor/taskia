f5298ce4f3032ef62baa7e5ad3e914df07d542cc
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-07-11 15:17:23 +0700

    layout components

--- src/main.js
+++ src/main.js
@@ -6,8 +6,13 @@ import vClickOutside from 'click-outside-vue3';
 import { router } from './router/router';
 import vue3GoogleLogin from 'vue3-google-login';
 import { store } from './store/store';
+import { layouts } from './components/layout';
 
 const app = createApp(App);
+
+for (const layout in layouts) {
+  app.component(layout, layouts[layout]);
+}
 
 app.use(MotionPlugin);
 app.use(vClickOutside);
